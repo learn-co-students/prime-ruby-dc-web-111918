83a495b2cb1192b6c374531f88abd857e1c897fc
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-17 20:51:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+require 'prime'
+
+def prime?(num)
+  Prime.prime?(num)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def prime?(number)
+ #(2..number-1).each do |num|
+  # if number % num == 0
+    # return false
+   # end
+  #end
+ # return true
+#end
